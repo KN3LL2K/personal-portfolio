992d439182d8bd2952130039f0c60e441c275bcb
Harry Bellenie <harrybellenie@gmail.com>
2017-02-18 23:08:37 -0800

    Adds background color change and active selector for project-menu-item's

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 $(document).ready(function() {
+
+
+  $('.project-menu-item').on('mouseenter', function() {
+    var elem = $(this);
+    var bg = $('#project-bg');
+    bg.removeClass();
+    console.log(elem[0].id)
+    if (elem[0].id === 'cryptocracy') {
+      bg.addClass('green');
+    } else if (elem[0].id === 'colorzio') {
+      bg.addClass('orange');
+    } else if (elem[0].id === 'deckstomp') {
+      bg.addClass('yellow');
+    } else {
+      bg.addClass('dBlue');
+    }
+    $('.project-menu-item').removeClass('-active');
+    $('.project-menu-item-dot').removeClass('-active-dot');
+    elem.addClass('-active');
+    elem.children('.project-menu-item-dot').addClass('-active-dot');
+  })
   
 
+
+  
 
 
 });
